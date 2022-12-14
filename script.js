1e10d08dffce1b993facb43ecbab240e0b6b7edc
kchhatrala30 <118632329+kchhatrala30@users.noreply.github.com>
2022-12-14 12:09:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 let scale = "imperial";
+let key = "4727f6a2078ba4ade6f8ab2691b946b7";
 let currentCity;
 
 const root = ReactDOM.createRoot(document.querySelector(".current-temperature"));
 
-function fetchWeather(city)
+function fetchLocation(city)
 {
     currentCity = city;
-    let key = "10aade587740d37f361ec56082945ae6";
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + scale + "&appid=" + key;
+    let geocoder = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=&appid=" + key;
 
-    fetch(url)
+    fetch(geocoder)
     .then(response => 
         response.json()
     )
     .then(data => {
+        const {lat, lon} = data[0];
+        fetchWeather(lat, lon);
+        displayLocation(data[0]);
+    });  
+}
+
+function fetchWeather(lat, lon)
+{
+    let url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&units=" + scale + "&exclude=minutely,hourly,alerts&appid=" + key;
+    
+    fetch(url)
+    .then(response =>
+        response.json()
+    )
+    .then(data => 
         displayWeather(data)
-    });
+        // displaySolar(data)
+    );
 }
 
 function displayWeather(data)
 {
-    // console.log(data);
-    let {name} = data;
-    let {country} = data.sys;
-    let {description, icon} = data.weather[0];
-    let {temp, feels_like} = data.main;
+    let {description, icon} = data.current.weather[0];
+    let {temp, feels_like} = data.current;
     temp = Math.round(temp);
     feels_like = Math.round(feels_like);
-
-    document.querySelector(".city-name").innerText = "Weather in " + name + ", " + country;
-    document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector(".description").innerText = description;
 
     if (scale == "metric") {
         document.querySelector(".current-temperature").innerText = temp + "°C";
@@ -40,6 +49,10 @@ function displayWeather(data)
         document.querySelector(".current-temperature").innerText = temp + "°F";
         document.querySelector(".feels-like").innerText = "Feels like: " + feels_like + "°F";
     }
+    
+    document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    
 
     document.querySelector(".main-info").classList.remove("loading");
 
@@ -73,15 +86,34 @@ function displayWeather(data)
     }
 }
 
+function displayLocation(data)
+{
+    let {name, state, country} = data;
+    if (state != undefined)
+    {
+        document.querySelector(".city-name").innerText = "Weather in " + name + ", " + state + ", " + country;
+    }
+    else {
+        document.querySelector(".city-name").innerText = "Weather in " + name + ", " + country;
+    }
+    
+}
+
+// function displaySolar(data)
+// {
+//     let {sunrise, sunset, moon_phase} = data.daily[0];
+//     document.querySelector(".sunrise-sunset").innerText = "Sunrise: " + sunrise + "\nSunset: " + sunset + "\nMoon phase: " + moon_phase;
+// }
+
 document.querySelector(".search-bar").addEventListener("keyup",  function (event) {
     if (event.key == "Enter") {
-        const element = fetchWeather(document.querySelector(".search-bar").value);
+        const element = fetchLocation(document.querySelector(".search-bar").value);
         root.render(element);
     }
 })
 
 document.querySelector(".search-location button").addEventListener("click", function () {
-    const element = fetchWeather(document.querySelector(".search-bar").value);
+    const element = fetchLocation(document.querySelector(".search-bar").value);
     root.render(element);
 })
 
@@ -92,7 +124,7 @@ document.querySelector(".current-temperature").addEventListener("click", functio
     else {
         scale = "metric";
     }
-    const element = fetchWeather(currentCity);
+    const element = fetchLocation(currentCity);
     root.render(element);
 })
 
@@ -103,8 +135,12 @@ document.querySelector(".feels-like").addEventListener("click", function () {
     else {
         scale = "metric";
     }
-    const element = fetchWeather(currentCity);
+    const element = fetchLocation(currentCity);
     root.render(element);
 })
 
-fetchWeather("New York");
+// document.querySelector(".solar-data button").addEventListener("click", function () {
+//     document.querySelector(".main-info").classList.remove("hidden");
+// })
+
+fetchLocation("Manhattan");
